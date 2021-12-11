315f5238aae3cd1e3f5aeb8ad6b8b12afa61e611
anmabe <80114034+anmabe06@users.noreply.github.com>
2021-12-11 21:14:32 +0100

    Changed player jump. Added score and lifes text

--- user_character.py
+++ user_character.py
@@ -4,21 +4,28 @@ class Character():
     def __init__(self, username, character):
         self.username = username
         self.lifes = 5
+        self.jump_speed = 20
+        self.speed = 500
+        self.score = 0
+
+        self.jumping_animation_duration = fps/2
+        self.jumping_animation_duration_halved = fps/4
+        self.jumping_animation_duration_timer = 0
 
         if(character == "melchor"):
             self.character = "melchor"
             self.character_image = "assets/images/melchor.png"
-            self.width, self.height = 140, 140
+            self.width, self.height = 100, 100
 
         elif(character == "gaspar"):
             self.character = "gaspar"
             self.character_image = "assets/images/gaspar.png"
-            self.width, self.height = 122, 147
+            self.width, self.height = 100, 100
 
         elif(character == "baltasar"):
             self.character = "baltasar"
             self.character_image = "assets/images/baltasar.png"
-            self.width, self.height = 122, 147
+            self.width, self.height = 100, 100
 
         else:
             self.character = "santa"
@@ -29,4 +36,3 @@ class Character():
         self.y = 700/2 - self.height
         self.initial_x = self.x
         self.initial_y = self.y
-            
